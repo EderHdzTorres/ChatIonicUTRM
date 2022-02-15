45ca646b1428c609665008be582c48ffc035c91d
Juan-DLP <59587935+Juan-DLP@users.noreply.github.com>
2022-02-15 14:44:50 -0500

    login signup

--- ChatUTRM/src/app/app-routing.module.ts
+++ ChatUTRM/src/app/app-routing.module.ts
@@ -3,17 +3,21 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'login',
+    path: '',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
   {
-    path: '',
+    path: 'chat',
     loadChildren: () => import('./pages/chat/chat.module').then( m => m.ChatPageModule)
   },
   {
     path: 'contactos',
     loadChildren: () => import('./pages/contactos/contactos.module').then( m => m.ContactosPageModule)
+  },  {
+    path: 'signup',
+    loadChildren: () => import('./pages/signup/signup.module').then( m => m.SignupPageModule)
   },
+
 ];
 
 @NgModule({
