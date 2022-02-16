2e97892f0207630853b18ab3202889e1a9d2a793
EderHdzTorres <98924484+EderHdzTorres@users.noreply.github.com>
2022-02-16 13:07:44 -0500

    Login y signUp funcionando

--- ChatUTRM/src/app/pages/login/login.page.ts
+++ ChatUTRM/src/app/pages/login/login.page.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage implements OnInit 
+{
 
-  constructor() { }
+  formularioLogin: FormGroup;
 
-  ngOnInit() {
+  constructor(public fb: FormBuilder,    public alertController:AlertController) {
+
+   this.formularioLogin = this.fb.group({
+      'nombre': new FormControl ("",Validators.required),
+      'password': new FormControl("",Validators.required)
+    })
+
+  }
+  async ingresar(){
+    var f = this.formularioLogin.value;
+
+    var usuario= JSON. parse(localStorage.getItem('usuario'));
+    if(usuario.nombre==f.nombre && usuario.password == f.password)
+    {
+       console.log('Ingresado');
+    }
+    else{
+       const alert = await this.alertController.create({
+         header: 'Datos incorrectos',                       
+         message: 'Los datos que ingresaste son incorrectos.',
+         buttons: ['Aceptar']
+       });
+      await alert.present();
+    }
+ 
   }
 
+  ngOnInit() 
+  {
+  }
 }
