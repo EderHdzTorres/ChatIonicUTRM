a7a29e590c5fa014efb292ec8f754171b90a4eb7
EderHdzTorres <98924484+EderHdzTorres@users.noreply.github.com>
2022-03-23 20:27:39 -0500

    Contactos

--- ChatUTRM/src/app/pages/contactos/contactos.page.ts
+++ ChatUTRM/src/app/pages/contactos/contactos.page.ts
@@ -1,42 +1,63 @@
-import { Component, OnInit } from '@angular/core';
-import { AlertController, NavController } from '@ionic/angular';
+import {Component, OnInit} from '@angular/core';
+import { SocketServiceService } from '../../services/socket-service.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-contactos',
   templateUrl: './contactos.page.html',
   styleUrls: ['./contactos.page.scss'],
 })
+
+// export interface User {
+//   id: number;
+//   username: string;
+//   password: string;
+//   socket_id: string;
+//   online: boolean;
+//   avatar: string;
+// };
+
 export class ContactosPage implements OnInit {
 
-  constructor(public alertController: AlertController, public navCtrl: NavController) { }
+  public onlineUsers = [];
+  // public user: User;
+  public userId: any;
+
+
+  constructor(
+    private socketService: SocketServiceService,
+    private route: ActivatedRoute,
+    private service: UserService,
+    private router: Router
+    ) {}
 
   ngOnInit() {
-  }
+      console.log('Me conctpppppppppppppppppppoooooooooooo');
 
-  //funcion para cerrar sesion
-  async salir(){
-    const alert = await this.alertController.create({
-      header: 'Salir',
-      message: '¿Seguro que quieres salir?',
-      buttons: [
-        {
-          text: 'No',
-          handler: () => {
+    this.userId = this.service.getId();
+    // console.log('init tab');
+    // this.user = this.route.snapshot.queryParams;
+    // console.log('init tab', this.user);
 
-          }
-        }, {
-          text: 'Si',
-          handler: () => {
-            localStorage.removeItem('ingresado');
-            localStorage.removeItem('TK1983!');
-            localStorage.removeItem('userId');
-            this.navCtrl.navigateRoot('');
+    // if(this.user){
+    //   this.user.id = parseInt(this.user.id, 10);
+    // }
 
-          }
-        }
-      ]
+    console.log('user in tab', this.userId);
+    this.socketService.getOnlineUsers().subscribe((users) => {
+      console.log('users connected', users);
+      if(users) {
+        this.onlineUsers = users;
+      }
     });
-    await alert.present();
   }
 
+  async navigateToChat(id) {
+    await this.router.navigate(['/home/tab2'], { queryParams: { userId: id } } );
+  }
+
+  hola(){
+    console.log('hola');
+  }
 }
